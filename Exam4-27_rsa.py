046c16b43acf6bbc6aef7fb5ff3a0785ce3204b6
AlpHerk <secra@qq.com>
2021-12-02 13:59:25 +0800

    main

--- Exam4-27_rsa.py
+++ Exam4-27_rsa.py
@@ -1,4 +1,8 @@
 from rsacipher import RSA, enCode, deCode
+import time
+
+
+startime = time.time()
 
 mscode = enCode("please wait for me")
 cicode = "0763222127199153452800748825533895624854"
@@ -8,6 +12,9 @@ cicode = crypto.encrypt(mscode)
 mscode = crypto.decrypt(cicode)
 messag = deCode(mscode)
 
+endtime = time.time()
+
 print("密文编码流为:", cicode)
 print("明文编码流为:", mscode)
 print("解密后的明文:", messag)
+print(endtime - startime)
